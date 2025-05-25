86681929838482b7270d4fe0d259984b3fe4a273
JR Gill <jr@beachfrontcustomhomes.com>
2025-05-25 14:40:36 -0400

    Switch to HashRouter for thank-you redirect support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import About from "./pages/About";
 import Services from "./pages/Services";
